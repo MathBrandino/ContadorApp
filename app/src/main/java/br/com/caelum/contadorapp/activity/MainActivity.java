8075532712252ee2cb4031ef4047885d6f1ddda0
Matheus Brandino <m.v.brandino@gmail.com>
2015-07-10 13:09:40 -0300

    projeto ContadorApp

---
+++ app/src/main/java/br/com/caelum/contadorapp/activity/MainActivity.java
@@ -0,0 +1,87 @@
+package br.com.caelum.contadorapp.activity;
+
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import br.com.caelum.contadorapp.R;
+import br.com.caelum.contadorapp.fragment.ListaFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Bundle bundle;
+    private String nome = "Nós";
+    private String nome1= "Eles";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bundle = savedInstanceState;
+
+        ListaFragment listaFragmentDireita = new ListaFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("nome", nome);
+        listaFragmentDireita.setArguments(bundle);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_direita, listaFragmentDireita);
+        transaction.commit();
+
+        ListaFragment listaFragmentEsquerda = new ListaFragment();
+        Bundle bundle1 = new Bundle();
+        bundle1.putSerializable("nome", nome1);
+        listaFragmentEsquerda.setArguments(bundle1);
+
+
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.frame_esquerda, listaFragmentEsquerda);
+        tx.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_zerar) {
+
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putSerializable("nome", nome);
+        outState.putSerializable("nome1", nome1);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        nome = (String) savedInstanceState.getSerializable("nome");
+        nome1 = (String) savedInstanceState.getSerializable("nome1");
+    }
+}
---
+++ app/src/main/java/br/com/caelum/contadorapp/fragment/ListaFragment.java
@@ -0,0 +1,159 @@
+package br.com.caelum.contadorapp.fragment;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.caelum.contadorapp.R;
+
+/**
+ * Created by matheus on 08/07/15.
+ *
+ *
+ */
+public class ListaFragment extends Fragment {
+
+    private String nome;
+    private ListView listView;
+    private TextInputLayout layout;
+    private EditText entradaValor;
+    private List<Integer> valores ;
+    private TextView resultadoSoma;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Bundle bundle = getArguments();
+        nome = (String) bundle.getSerializable("nome");
+
+        View view = inflater.inflate(R.layout.item_contador, container, false);
+
+        valores = new ArrayList<>();
+        TextView nomeTextView = (TextView) view.findViewById(R.id.nome_text);
+
+        nomeTextView.setText(nome);
+
+        entradaValor = (EditText) view.findViewById(R.id.entra_valor_contagem);
+
+        layout = (TextInputLayout) entradaValor.getParent();
+
+        listView = (ListView) view.findViewById(R.id.lista_contador);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(getActivity()).setTitle("Remover : " + valores.get(position).toString() + "?")
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                valores.remove(position);
+                                onResume();
+                            }
+                        })
+                        .setNegativeButton("Não", null).show();
+
+                return false;
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!entradaValor.getText().toString().trim().isEmpty()) {
+                    String valor = entradaValor.getText().toString();
+
+                    valores.add(Integer.valueOf(String.valueOf(valor)));
+
+                    entradaValor.getText().clear();
+
+                    onResume();
+                } else {
+                    layout.setError("Precisa de um valor");
+                }
+            }
+        });
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab2);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valores.clear();
+                onResume();
+            }
+        });
+
+
+        resultadoSoma = (TextView) view.findViewById(R.id.soma);
+
+
+
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        int soma = 0;
+        if (valores.size() > 0) {
+            for (int valor : valores) {
+
+                int somaNovo = valor;
+
+                somaNovo = soma + somaNovo;
+
+                soma = somaNovo;
+
+
+            }
+        }
+
+        resultadoSoma.setText(String.valueOf(soma));
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getActivity(), android.R.layout.simple_list_item_1, valores);
+
+        listView.setAdapter(adapter);
+
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putSerializable("valores", (Serializable) valores);
+    }
+
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+
+        if(savedInstanceState != null) {
+            valores = (List<Integer>) savedInstanceState.getSerializable("valores");
+
+        }
+    }
+}
