27115ea2bdb3229aa7d319f69c09e145415733c6
Matheus Brandino <m.v.brandino@gmail.com>
2015-07-20 15:58:16 -0300

    correcao de pacotes

--- app/src/main/java/br/com/caelum/contadorapp/activity/MainActivity.java
+++ app/src/main/java/br/com/caelum/contadorapp/activity/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
